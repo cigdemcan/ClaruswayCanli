e0196854ac11d01f0a527256a58b6d159d1b845b
Çağla Cengiz <cigdem18701986@gmail.com>
2022-10-08 17:23:05 +0300

    07.10.2022

--- src/day2/Q07.java
+++ src/day2/Q07.java
@@ -1,5 +1,7 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Q07 {
 
   /*
@@ -9,4 +11,21 @@ public class Q07 {
         18 yasindann buyuk ve 50 kilo dan hafif ise kan bagisi yapamaz.
         18 yasindan buyuk ve 50 ve 50 kilodan agir ise kan bagisi yapabilir.
          */
+  public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      System.out.println("yasınızı giriniz" );
+      int yas=sc.nextInt();
+      System.out.println("kilonuzu giriniz");
+      int kilo=sc.nextInt();
+      if(yas>0&& yas<18){
+          System.out.println("yası 18`den küçük olanlar kan bagışı yapamaz ");
+      } else if (yas>=18) {
+          if(kilo>0&& kilo<50){
+              System.out.println("kilonuz 50`den küçük olduğu için kan bağışı yapamaz");
+
+          }else {
+              System.out.println("kan bagışı yapabilirsiniz");
+          }
+      }
+  }
 }
